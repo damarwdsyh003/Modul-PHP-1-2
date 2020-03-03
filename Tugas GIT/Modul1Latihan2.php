fffae3d50421f0b158960504139bb8bb4129001c
damarwdsyh003 <53498693+damarwdsyh003@users.noreply.github.com>
2020-03-03 17:46:25 +0700

    Add files via upload

---
+++ Tugas GIT/Modul1Latihan2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP dalam tag HTML</title>
+  </head>
+  <body>
+    <?php
+      echo "Selamat Datang di SMK Telkom Sandhy Putra Malang";
+     ?>
+  </body>
+</html>
